34b84f0fef2dd9ab5fb49a242301d28dc8824def
Jose Ismael <jidp97@gmail.com>
2024-02-11 19:59:00 -0400

    tiempo real signalR

---
+++ QuotaInvoice/Shared/Models/HubMessagesType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuotaInvoice.Shared.Models
+{
+    public class HubMessagesType
+    {
+        public const string Modificado = "MessageEdit";
+        public const string Eliminado = "MessageDelete";
+        public const string Agregado = "SendMessage";
+    }
+}
